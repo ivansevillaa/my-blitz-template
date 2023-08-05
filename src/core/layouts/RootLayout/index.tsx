65ccf2e5f7e58c24a37699c3668f60c3d4ef4400
Ivan Sevilla <ivansevillaa@gmail.com>
2023-08-05 13:42:15 -0300

    add shell and style link

--- src/core/layouts/RootLayout/index.tsx
+++ src/core/layouts/RootLayout/index.tsx
@@ -2,12 +2,15 @@ import Head from "next/head";
 
 import { BlitzLayout } from "@blitzjs/next";
 
+import { AppShell, Footer, Header, List, Text } from "@mantine/core";
 import React from "react";
 
 const RootLayout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
   title,
   children,
 }) => {
+  const year = new Date().getFullYear();
+
   return (
     <>
       <Head>
@@ -15,7 +18,20 @@ const RootLayout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = 
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {children}
+      <AppShell
+        header={
+          <Header height={60}>
+            <Text>Logo</Text>
+          </Header>
+        }
+        footer={
+          <Footer height={120}>
+            <Text>Copyright @ {year}</Text>
+          </Footer>
+        }
+      >
+        {children}
+      </AppShell>
     </>
   );
 };
