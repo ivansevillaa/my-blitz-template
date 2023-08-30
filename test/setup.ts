e8cb84021e40cf4f7fa8c5a79c4e677ae97b6bb8
Ivan Sevilla <ivansevillaa@gmail.com>
2023-08-30 20:26:27 -0300

    mock env vars for test environment

--- test/setup.ts
+++ test/setup.ts
@@ -1,3 +1,12 @@
 import "@testing-library/jest-dom";
+import { vi } from "vitest";
+
+vi.mock("src/env.mjs", () => ({
+  env: {
+    NODEMAILER_LOCAL_USER: "test-user",
+    NODEMAILER_LOCAL_PASS: "test-pass",
+    RESEND_API_KEY: "api-key",
+  },
+}));
 
 export {};
